1ce773f99e65ed91a6cc2161581b7fdb901971bd
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-23 13:57:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,4 @@
 class Artist 
   attr_accessor :name 
   
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-  
-  def add_song(song)
-  @songs << song
-  song.artist= self
-end
-  
 end
